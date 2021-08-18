7c84fba569aa882547d24aa984c32458faf9825a
gracexuerwu <grace.xuer.wu@gmail.com>
2021-08-18 16:43:14 +0200

    Lottie if else test

--- src/WeatherLottie.js
+++ src/WeatherLottie.js
@@ -7,36 +7,37 @@ import weatherCloudy from "./animation/cloudy.json";
 
 
 export default function WeatherLottie(props) {
-    const codeMapping = {
-        "01d": "CLEAR_DAY",
-        "01n": "CLEAR_NIGHT",
-        "02d": "PARTLY_CLOUDY_DAY",
-        "02n": "PARTLY_CLOUDY_NIGHT",
-        "03d": "PARTLY_CLOUDY_DAY",
-        "03n": "PARTLY_CLOUDY_NIGHT",
-        "04d": "CLOUDY",
-        "04n": "CLOUDY",
-        "09d": "RAIN",
-        "09n": "RAIN",
-        "010d": "RAIN",
-        "010n": "RAIN",
-        "011d": "CLEAR_DAY",
-        "011n": "CLEAR_NIGHT",
-        "013d": "SNOW",
-        "013n": "SNOW",
-        "050d": "FOG",
-        "050n": "FOG",
-    };
-    
-    let weatherIcon;
-    // if (props.code === "04d" || props.code === "04n") {
-    //   weatherIcon = weatherCloudy;
-    // } 
-    // else if () {
-
-    // }
+  // const codeMapping = {
+  //   "01d": "CLEAR_DAY",
+  //   "01n": "CLEAR_NIGHT",
+  //   "02d": "PARTLY_CLOUDY_DAY",
+  //   "02n": "PARTLY_CLOUDY_NIGHT",
+  //   "03d": "PARTLY_CLOUDY_DAY",
+  //   "03n": "PARTLY_CLOUDY_NIGHT",
+  //   "04d": "CLOUDY",
+  //   "04n": "CLOUDY",
+  //   "09d": "RAIN",
+  //   "09n": "RAIN",
+  //   "010d": "RAIN",
+  //   "010n": "RAIN",
+  //   "011d": "CLEAR_DAY",
+  //   "011n": "CLEAR_NIGHT",
+  //   "013d": "SNOW",
+  //   "013n": "SNOW",
+  //   "050d": "FOG",
+  //   "050n": "FOG",
+  // };
+  let weatherIcon;
+  if (props.code === "04d" || props.code === "04n") {
     weatherIcon = weatherCloudy;
-    return (
-      <Lottie animationData={weatherIcon} />
-    );
+  }
+  else if (props.code === "05d" || props.code === "05n") {
+    weatherIcon = weatherCloudy;
+  }
+  else {
+    weatherIcon = weatherCloudy;
+  }
+  return (
+    <Lottie animationData={weatherIcon} />
+  );
 }
